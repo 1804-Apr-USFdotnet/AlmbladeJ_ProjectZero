cde9b0465bfc253f1e7f31c5fb7f6780f33649b5
jalmblade <j.almblade@gmail.com>
2018-04-23 12:14:25 -0400

    Added a few interfaces

---
+++ Project0/ReviewFunctions/IReview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantFunctions;
+
+namespace ReviewFunctions
+{
+    interface IReview
+    {
+        int Rating { get; set; }
+        string ReviewText { get; set; }
+
+    }
+}
